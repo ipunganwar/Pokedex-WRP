ffca37eb83d1de6110484d387b0187baf549933e
ipung anwar <ipung.anwar1@gmail.com>
2018-12-08 02:53:11 +0700

    react-navigation

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react'
+import { Platform } from 'react-native'
+import { createAppContainer } from 'react-navigation'
+import { Provider } from 'react-redux'
+import { ApolloProvider } from "react-apollo"
+import store from './src/store'
+import client from './src/client'
+import AppNavigator from './src/screens/Navigator'
 
-export default class App extends React.Component {
+const AppContainer = createAppContainer(AppNavigator)
+console.disableYellowBox = true;
+
+
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+      <ApolloProvider client={client}>
+        <Provider store={store}>
+          <AppContainer />
+        </Provider>
+      </ApolloProvider>
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
